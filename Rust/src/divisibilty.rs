bc9afd7c13139602109ea62be05ff673015cc00b
Chris Negrich <cnegrich@gmail.com>
2020-02-01 11:14:04 -0700

    Stubbed out some divisibility functions

---
+++ Rust/src/divisibilty.rs
@@ -0,0 +1,35 @@
+//! Divisibility Functions
+// Author: Christopher Negrich
+// Github: cnegrich
+//
+// Some functions on divisibility
+
+/// Primality
+///
+/// Check if a number is prime
+fn primality(n: usize) -> bool {
+    //
+}
+
+/// Relative Primality
+///
+/// Check if two numbers are relatively prime
+fn relative_primality(n: usize, m: usize) -> bool {
+    //
+}
+
+/// Greatest Commmon Divisor
+///
+/// Find the GCD of two numbers
+fn gcd(n: usize, m: usize) -> usize {
+    //
+}
+
+// Unit Tests ------------------------------------------------------------------
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_() {}
+}
--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,4 +1,6 @@
 mod fibonacci;
+mod multiples;
+//mod divisibility;
 
 fn main() {
     println!("TODO: Put a CLI here using clap. Have it run benchmarks.");
