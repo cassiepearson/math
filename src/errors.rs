89b9141515fd1d7a3d849380cc7da384390c5134
Cassie Pearson <cnegrich@gmail.com>
2022-03-30 19:51:42 -0700

    Feat/gh actions update (#12)
    * Added error handling and extended euclidean
    
    Added a file to begin working on error handling and checked operations.
    Added a fully implemented extended euclidean algorithm including
    functions to return only the egcd and bezout coefficients.
    
    * Added euclidean algs to mod
    
    * Switched to stable release
    
    All checks, formatting, and builds were switched over to the stable
    version of Rust.
    
    * Updated my name
    
    * Chore: Update
    
    * fmt
    
    * Updated name to stable
    
    * Temp: dead code allowed

---
+++ src/errors.rs
@@ -0,0 +1,16 @@
+//! Errors enum
+
+/// Errors enumeration
+///
+/// Lists all possible errors that can occur during the execution of the program
+#[allow(dead_code)]
+enum LibraryError {
+    InvalidOperation(String),
+    InvalidNumber(String),
+    Parser(String),
+    Other(String),
+}
+
+// TODO: implements error types for each module
+// TODO: thiserror and anyhow integration
+// TODO: Checked operations throughout the code
