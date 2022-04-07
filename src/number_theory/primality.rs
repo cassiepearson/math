4b3f9880d78438d09dc3fd1c83cda75d2d0ac7b6
Christopher Negrich <cnegrich@gmail.com>
2022-04-07 07:37:36 -0700

    Updated Wilsons to use custom error
    Separated the wilson's primality check to its own macro. It uses
    factorial and may fail. Now it returns a MathLibError instead of
    potentially panicking.

--- src/number_theory/primality.rs
+++ src/number_theory/primality.rs
@@ -1,18 +1,26 @@
 //! Primality Trait
 use crate::{
-    general::numbers::Integer,
+    general::{errors::MathLibError, numbers::Integer},
     number_theory::{factorial::Factorial, gcd::Gcd},
 };
-use anyhow::Result;
+use std::fmt::Display;
+
+type Result<T> = std::result::Result<T, MathLibError>;
 
 /// Primality Trait
 ///
 /// Implements various functions covering primality tests and prime factorizations
 pub trait Primality<T> {
-    fn wilson(self) -> Result<bool>;
     fn primality(self) -> bool;
     fn relative_primality(self, other: T) -> bool;
     fn prime_factorization(self) -> Vec<T>;
+}
+
+/// Wilson's Primality Trait
+///
+/// Implements factorial-based primality tests
+pub trait WilsonsPrimality<T> {
+    fn wilson(self) -> Result<bool>;
 }
 
 #[macro_export]
@@ -22,17 +30,6 @@ macro_rules! impl_primality {
         where
             T: $t,
         {
-            /// Wilson's Theorem to test Primality
-            ///
-            /// Only use this if you are using small integers. Actually, don't use this at all. Wilson's theorem is never practical to compute but has implications in theory
-            fn wilson(self) -> Result<bool> {
-                // Rust can perform a remainder operation, but not a modular operation
-                match (self - T::one()).factorial() {
-                    Ok(val) => Ok((val % self) + T::one() + T::one() == ((T::one()) % self) + self),
-                    Err(error) => Err(error),
-                }
-            }
-
             /// Primality
             ///
             /// Check if a number is prime
@@ -103,27 +100,64 @@ macro_rules! impl_primality {
         }
     };
 }
+
+#[macro_export]
+macro_rules! impl_wilsons_primality {
+    ($t: ident) => {
+        impl<T> WilsonsPrimality<T> for T
+        where
+            T: $t + Display,
+        {
+            /// Wilson's Theorem to test Primality
+            ///
+            /// Only use this if you are using small integers. Actually, don't use this at all. Wilson's theorem is never practical to compute but has implications in theory
+            fn wilson(self) -> Result<bool> {
+                // Rust can perform a remainder operation, but not a modular operation
+                match (self - T::one()).factorial() {
+                    Ok(val) => Ok((val % self) + T::one() + T::one() == ((T::one()) % self) + self),
+                    Err(error) => Err(error),
+                }
+            }
+        }
+    };
+}
+
 impl_primality!(Integer);
+impl_wilsons_primality!(Integer);
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use rstest::rstest;
 
-    #[rstest]
-    #[case(2, true)]
-    #[case(3, true)]
-    #[case(5, true)]
-    #[case(15, false)]
+    #[rstest(
+        case::prime_1(2, true), 
+        case::prime_2(3, true), 
+        case::prime_3(5, true),
+        case::non_prime_1(15, false),
+        case::non_prime_2(4, false),
+        case::non_prime_3(9, false),
+        #[should_panic]
+        case::should_panic_1(22, false),
+        #[should_panic]
+        case::should_panic_2(29, true)
+    )]
     fn usize_wilson_test(#[case] a: usize, #[case] expected: bool) {
         assert_eq!(expected, a.wilson().unwrap())
     }
 
-    #[rstest]
-    #[case(2, true)]
-    #[case(3, true)]
-    #[case(5, true)]
-    #[case(15, false)]
+    #[rstest(
+        case::prime_1(2, true), 
+        case::prime_2(3, true), 
+        case::prime_3(5, true),
+        case::non_prime_1(15, false),
+        case::non_prime_2(4, false),
+        case::non_prime_3(9, false),
+        #[should_panic]
+        case::should_panic_1(22, false),
+        #[should_panic]
+        case::should_panic_2(29, true)
+    )]
     fn isize_wilson_test(#[case] a: isize, #[case] expected: bool) {
         assert_eq!(expected, a.wilson().unwrap())
     }
@@ -131,7 +165,7 @@ mod tests {
     #[test]
     #[should_panic]
     fn wilson_overflow_test() {
-        let out = 1000.wilson().unwrap();
+        1000.wilson().unwrap();
     }
 
     #[rstest]
